a3d077048af6740b82f9aab43b319a4aa3b42322
MolyaKsu <ksushamolya@icloud.com>
2023-01-29 16:40:06 -0800

    node lesson4

---
+++ index.js
@@ -0,0 +1,6 @@
+const fs =  require('fs');
+const yargs = require('yargs');
+const readline = require('readline');
+//const colors = require('colors');
+//const [from, to] = process.argv.slice(2);
+
